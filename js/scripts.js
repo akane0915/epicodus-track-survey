85b7020adc91f2b541526c68417835b196b46853
akane0915 <asialkane@gmail.com>
2017-03-24 15:25:41 -0700

    add error message logic for empty name field by using .show and .addClass.  Add comments to scripts.js file for better readability

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,32 @@
-// Backend Logic
-
-// Frontend Logic
+// User Interface Logic
 $(function(){
 
-  $("form").submit(function(event){
+  $("form").submit(function(event){  // Click to submit form
     event.preventDefault();
+    // Declare variables
     var personalityInput = $("#personality").val();
     var workInput = $("#work").val();
     var nameInput = $("#name").val();
 
+    // Error message if Name Question is left blank
+    if(nameInput === ""){
+      $(".error-message").show();
+      $(".error-message").addClass("error-class");
+      return;
+    }
+
+    // Hide results section if form is re-submitted
     $("#ruby").hide();
     $("#css").hide();
     $("#csharp").hide();
     $("#no-track").hide();
     $("#php").hide();
 
+    // Display user name in results
     $(".name").text(nameInput);
 
+    // Branching logic for results based on user input
+    // PersonalityInput Question
     if (personalityInput === "Creative, Design-oriented"){
         $("#css").fadeIn(2000);
     } else if (personalityInput === "Entrepreneurial, Open-minded"){
@@ -26,6 +36,7 @@ $(function(){
     } else if (personalityInput === "A person who absolutely hates computers and technology"){
       $("#no-track").fadeIn(2000);
     } else {
+      // WorkInput Question
       if (workInput === "A large, established company"){
         $("#csharp").fadeIn(2000);
       } else if (workInput === "A small to medium size company or startup"){
@@ -34,7 +45,7 @@ $(function(){
         $("#css").fadeIn(2000);
       } else {
       $("#php").fadeIn(2000);
-      }
-    }
+      } // Close of WorkInput if statement
+    } // Close of PersonalityInput if statement
   }); // Form submit function close
-}); // Frontend Document Ready function close
+}); // User Interface Document Ready function close
