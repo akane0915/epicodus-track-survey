6474d9c518b1b3f69ee523c077baaf2749279be8
akane0915 <asialkane@gmail.com>
2017-03-24 10:44:09 -0700

    add front end logic to scripts.js to collect user input for questions 1-3 and save as variables

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,15 @@
 
 // Frontend Logic
 $(function(){
-
+  $("form").submit(function(event){
+    event.preventDefault();
+    var personalityInput = $("#personality").val();
+    var workInput = $("#work").val();
+    var roomInput = $("#room").val();
+    console.log(personalityInput);
+    console.log(workInput);
+    console.log(roomInput);
+  });
 
 
 
